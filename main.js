6121148e7df94d8399312c0ab23549ffdc911c3d
Manuel Herdez <herdez@gmail.com>
2023-04-09 17:39:31 -0600

    formulario

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import {collection, addDoc, getDocs} from 'firebase/firestore'
 import {db} from './src/firebase'
 import { questions } from './src/questions'
 
-import './style.css'
-
 const container = document.querySelector('#app')
 
 const addQuestions = () => questions.map( async question => {
@@ -14,14 +12,7 @@ const addQuestions = () => questions.map( async question => {
 
 
 
-container.innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <button id="btnAdd">Añadir preguntas a Firebase</button>
-  </div>
-`
-const btnAdd = document.querySelector('#btnAdd');
-btnAdd.addEventListener('click', addQuestions);
+
 
 const querySnapshot = await getDocs(collection(db, "questions"));
 querySnapshot.forEach((doc) => {
@@ -29,11 +20,37 @@ querySnapshot.forEach((doc) => {
   // container.innerHTML += `<h4>${doc.id}</h4>`
   // <pre> ${JSON.stringify(doc.data(), undefined, 2)}</pre>
 
-  container.innerHTML += `<h4>${doc.data().question}</h4>`
+  container.innerHTML += `<h4>Pregunta: ${doc.data().question}</h4>`
+  container.innerHTML += `<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Respuesta</th>
+      <th scope="col">Puntos</th>
+    </tr>
+  </thead>
+  `
   doc.data().answers.sort( (a,b) => b.points - a.points).map( ({answer, points}) => {
-    container.innerHTML += `<div>
-    <p>${answer} - <span>${points}</span></p>
-    </div>`
-  })
+    // container.innerHTML += `<table>
+    // <p>${answer} - <span>${points}</span></p>
+    // </table>`
+    container.innerHTML += `
+      <tbody>
+      <tr>
+        <td>${answer}</td>
+        <td>${points}</td>
+        </tr>
+        </tbody>
+        `
+
+    })
+    container.innerHTML += `
+  </table>`
 });
 
+container.innerHTML += `
+  <div>
+    <button id="btnAdd" class="btn btn-success">Añadir preguntas a Firebase</button>
+  </div>
+`
+const btnAdd = document.querySelector('#btnAdd');
+btnAdd.addEventListener('click', addQuestions);
